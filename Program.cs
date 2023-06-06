1e854649dc6e9d33ffd34b1b1c0d7a6cb0251dd8
MaximFadeev99 <135737222+MaximFadeev99@users.noreply.github.com>
2023-06-06 21:25:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSTest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //задача 1
+            byte lvl = 1;
+            sbyte stamina = 50;
+            short curseEffect = -70;
+            ushort armor = 600;
+            uint money = 130000;
+            int paymentBalance = -9500;
+            float oxygenReductionRate = -2.5f;
+            double expPerKill = 10.7;
+            char villageEmblem = '!';
+            string victoryNote = "Вы победили!";
+            bool death = false;
+
+
+
+
+
+
+
+
+        }
+    }
+}
