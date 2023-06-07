8ed7f533111c56134f2ef8b52c26fabc76f917c6
MaximFadeev99 <135737222+MaximFadeev99@users.noreply.github.com>
2023-06-07 15:39:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,18 +11,18 @@ namespace CSTest1
         static void Main(string[] args)
         {
             //задача 1
-            byte lvl = 1;
+            byte level = 1;
             sbyte stamina = 50;
             short curseEffect = -70;
             ushort armor = 600;
             uint money = 130000;
             int paymentBalance = -9500;
             float oxygenReductionRate = -2.5f;
-            double expPerKill = 10.7;
+            double experiencePerKill = 10.7;
             char villageEmblem = '!';
             string victoryNote = "Вы победили!";
             bool isDead = false;
-            bool staminaIsDepleted = true;
+            bool isWorking = true;
         }
     }
 }
